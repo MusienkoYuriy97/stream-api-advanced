ab64df6d771d2ecf7fe619698ee8357b2522bf3c
Pavel Lukyanovich <paellukyanovich1@gmail.com>
2023-08-25 09:24:53 +0300

    T-2-streamAPI: implementation methods

--- src/main/java/com/solbeg/Main.java
+++ src/main/java/com/solbeg/Main.java
@@ -2,17 +2,19 @@ package com.solbeg;
 
 
 import com.solbeg.exception.EntityNotFoundException;
-import com.solbeg.exception.ExerciseNotCompletedException;
 import com.solbeg.model.User;
 import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.Month;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class Main {
@@ -24,7 +26,8 @@ public class Main {
      * @return user with max balance wrapped with optional
      */
     public Optional<User> findRichestUser() {
-        throw new ExerciseNotCompletedException();
+        return users.stream()
+                .max(Comparator.comparing(User::getBalance));
     }
 
     /**
@@ -34,7 +37,9 @@ public class Main {
      * @return a list of Users
      */
     public List<User> findUsersByBirthdayMonth(Month birthdayMonth) {
-        throw new ExerciseNotCompletedException();
+        return users.stream()
+                .filter(user -> user.getBirthDay().getMonth().equals(birthdayMonth))
+                .toList();
     }
 
 
@@ -45,7 +50,8 @@ public class Main {
      * @return Map<String, List < User>> where key is email domain and value is a list of users
      */
     public Map<String, List<User>> groupUsersByEmailDomain() {
-        throw new ExerciseNotCompletedException();
+        return users.stream()
+                .collect(Collectors.groupingBy(user -> user.getEmail().split("@")[1]));
     }
 
 
@@ -53,14 +59,18 @@ public class Main {
      * @return total balance of all users
      */
     public BigDecimal calculateTotalBalance() {
-        throw new ExerciseNotCompletedException();
+        return users.stream()
+                .map(User::getBalance)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
     /**
      * @return list of users sorted by first and last names
      */
     public List<User> sortByFirstAndLastNames() {
-        throw new ExerciseNotCompletedException();
+        return users.stream()
+                .sorted(Comparator.comparing(User::getFirstName).thenComparing(User::getLastName))
+                .toList();
     }
 
     /**
@@ -68,7 +78,8 @@ public class Main {
      * @return true if there is a user that has an email with provided domain
      */
     public boolean containsUserWithEmailDomain(String emailDomain) {
-        throw new ExerciseNotCompletedException();
+        return users.stream()
+                .anyMatch(user -> user.getEmail().split("@")[1].equals(emailDomain));
     }
 
     /**
@@ -79,7 +90,11 @@ public class Main {
      * @return user balance
      */
     public BigDecimal getBalanceByEmail(String email) {
-        throw new ExerciseNotCompletedException();
+        return users.stream()
+                .filter(user -> Objects.equals(user.getEmail(), email))
+                .findFirst()
+                .orElseThrow(() -> new EntityNotFoundException("Cannot find User by email=" + email))
+                .getBalance();
     }
 
     /**
@@ -88,7 +103,8 @@ public class Main {
      * @return map of users by its ids
      */
     public Map<Long, User> collectUsersById() {
-        throw new ExerciseNotCompletedException();
+        return users.stream()
+                .collect(Collectors.toMap(User::getId, user -> user));
     }
 
 
@@ -99,7 +115,9 @@ public class Main {
      * @return a map where key is a last name and value is a set of first names
      */
     public Map<String, Set<String>> groupFirstNamesByLastNames() {
-        throw new ExerciseNotCompletedException();
+        return users.stream()
+                .collect(Collectors.groupingBy(User::getLastName,
+                        Collectors.mapping(User::getFirstName, Collectors.toSet())));
     }
 }
 
